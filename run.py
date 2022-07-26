7ccb5b5353cafb921e0d511384c3202be9872467
FranKBrownn <Frankcdbrown97@gmail.com>
2022-07-26 12:46:46 +0000

    added requirement to user data as well as an error message if it failes

--- run.py
+++ run.py
@@ -23,5 +23,29 @@ def get_sales_data():
     data_str = input("Enter your data here: ")
     print(f"The data provided is {data_str}")
 
+    sales_data = data_str.split(",")
+    print(sales_data)
+    validate_data(sales_data)
+
+
+
+def validate_data(values) :
+    print(values)
+
+    """
+    converts all str values to intergers
+    """
+
+    try:
+        if len(values) !=6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:     
+        print(f"invalid data {e} please try again.\n")   
+
 
 get_sales_data()   
+
+
+
