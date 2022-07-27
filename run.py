5a71324b31bdebeb908a4328e4ad6ea6b488e1d9
FranKBrownn <Frankcdbrown97@gmail.com>
2022-07-27 11:51:07 +0000

    created a while loop to repeat asking the same question to the user utnil the user imputs valed data

--- run.py
+++ run.py
@@ -17,15 +17,19 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+        data_str = input("Enter your data here: ")       
+        sales_data = data_str.split(",")
+        validate_data(sales_data)
 
-    data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+        if validate_data(sales_data):
+            print("data is valid")
+            break
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+    return sales_data            
 
 
 def validate_data(values):
@@ -40,10 +44,13 @@ def validate_data(values):
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:     
-        print(f"invalid data {e} please try again.\n")   
+        print(f"invalid data {e} please try again.\n")  
+        return False
+
+    return True         
 
 
-get_sales_data()   
+date = get_sales_data()   
 
 
 
